6cdc08879113240adbd624fc00d5f2be7fbb93c4
anzam <e.g.belyansky@gmail.com>
2025-08-20 13:40:52 +0400

    initial commit

---
+++ src/main/java/org/grtgb/coursework_2_javacore/Coursework2JavaCoreApplication.java
@@ -0,0 +1,13 @@
+package org.grtgb.coursework_2_javacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework2JavaCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coursework2JavaCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/grtgb/coursework_2_javacore/Coursework2JavaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grtgb.coursework_2_javacore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework2JavaCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
